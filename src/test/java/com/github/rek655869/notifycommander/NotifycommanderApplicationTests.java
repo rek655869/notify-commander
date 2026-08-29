7dd4f0d75860da810c3fcd60987e01eeeead2b8c
rek655869 <rek655869@gmail.com>
2026-08-29 17:59:16 +0000

    Initial commit

---
+++ src/test/java/com/github/rek655869/notifycommander/NotifycommanderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rek655869.notifycommander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifycommanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
